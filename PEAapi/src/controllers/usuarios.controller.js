4b7d6031008062ebd9c105c0fdee0e2bc14be91b
MarioGranados505 <114844105+MarioGranados505@users.noreply.github.com>
2022-10-17 18:25:03 -0600

    Controlador Usuarios completo
    controlador usuarios con todos los metodos

--- PEAapi/src/controllers/usuarios.controller.js
+++ PEAapi/src/controllers/usuarios.controller.js
@@ -1,8 +1,19 @@
+import { json } from "express"
 import { pool } from "../db.js"
 
 export const getUsuarios = async (req, res) => {
     const [rows] = await pool.query('Select * FROM usuarios')
     res.json(rows)
+}
+
+export const getUsuario = async (req, res) => {
+    const [rows] = await pool.query('Select * FROM usuarios WHERE IdUsuario = ?', [req.params.id])
+
+    if (rows.length <= 0) return res.status(404).json({
+        message: 'Valor no encontrado'
+    })
+
+    res.json(rows[0])
 }
 
 export const createUsuario = async(req, res) => {
@@ -11,6 +22,30 @@ export const createUsuario = async(req, res) => {
     res.send({ rows })
 }
 
-export const updateUsuario = (req, res) => res.send('actualizando usuario')
+export const deleteUsuario = async (req, res) => {
 
-export const deleteUsuarios = (req, res) => res.send('eliminando  usuario')
+    const [result] = await pool.query('DELETE FROM usuarios where IdUsuario = ?', [req.params.id])
+
+    if(result.affectedRows <= 0) return res.status(404).json({
+        message: 'Valor no entontrado'
+    })
+
+    res.send('Valor eliminado')
+}
+
+export const updateUsuario = async (req, res) => {
+    const{ id } = req.params
+    const{ Nombre, Apellido, Correo, Password} = req.body
+    
+    const [result] = await pool.query(
+        'UPDATE usuarios SET Nombre = IFNULL(?, Nombre), Apellido = IFNULL(?, Apellido), Correo = IFNULL(?, Correo), Password = IFNULL(?, Password) Where IdUsuario = ?',[Nombre, Apellido, Correo, Password, id]
+    )
+
+    if(result.affectedRows === 0) return res.status(404).json({
+        message: 'Valor no encontrado'
+    })
+
+    const [rows] = await pool.query('SELECT * FROM Usuarios WHERE IdUsuario = ?', [id])
+
+    res.json(rows[0])
+}
--- PEAapi/src/routes/usuarios.routes.js
+++ PEAapi/src/routes/usuarios.routes.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
-import {getUsuarios, createUsuario, updateUsuario, deleteUsuarios} from '../controllers/usuarios.controller.js'
+import {getUsuarios, createUsuario, updateUsuario, deleteUsuario, getUsuario} from '../controllers/usuarios.controller.js'
 
 const router = Router()
 
 router.get('/usuarios', getUsuarios)
 
+router.get('/usuarios/:id', getUsuario)
+
 router.post('/usuarios', createUsuario)
 
-router.put('/usuarios', updateUsuario)
+router.patch('/usuarios/:id', updateUsuario)
 
-router.delete('/usuarios', deleteUsuarios)
+router.delete('/usuarios/:id', deleteUsuario)
 
 export default router
