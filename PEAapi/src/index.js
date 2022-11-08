e46091b52de0ef6e3ef002e854475620e89a9c93
MarioGranados505 <114844105+MarioGranados505@users.noreply.github.com>
2022-11-08 15:38:49 -0700

    Correcion ejecucion api
    corregido la ejecucion de la api en el proyecto principal

--- PEAapi/src/index.js
+++ PEAapi/src/index.js
@@ -1,11 +1,5 @@
 import app from './app.js'
 import {PORT} from './config.js'
-import express from 'express'
-import {pool} from './db.js'
-import usuarios from  './routes/usuarios.routes.js'
-import clases from  './routes/clases.routes.js'
-import materialclase from  './routes/materialclase.routes.js'
-import clasesusuario from  './routes/clasesusuarios.routes.js'
 
 app.listen(PORT)
 console.log('server corriendo y escribiendo', PORT)
